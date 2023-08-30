d0a0893d51625b40e7735e3f40ff6333a33b2ff1
vustinchdev <vustinchdev@gmail.com>
2023-08-30 10:18:16 +0300

    add theme

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        primaryBg: '#FAFAFA',
+        secondaryBg: '#F5F2FD',
+        accent: '#5222D0',
+
+        font: '1F2626',
+        titleFont: '#151717'
+    }
+}
