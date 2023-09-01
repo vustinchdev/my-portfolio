9234237bc62fa2e373be6e3b85a0eb52fd917358
vustinchdev <vustinchdev@gmail.com>
2023-09-01 13:58:34 +0300

    stylized Contacts

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -4,6 +4,9 @@ export const theme = {
         secondaryBg: '#F5F2FD',
         accent: '#5222D0',
 
+        borderColor: '#534084',
+        placeHolderColor: '#919B9B',
+
         primaryFont: '#1F2626',
         secondaryFont: '#DBFFFF',
         titleFont: '#151717'
