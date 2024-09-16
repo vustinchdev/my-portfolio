c3a6c2733132d13f6b9143c1e464cf1b04447d4d
vustinchdev <vustinchdev@gmail.com>
2024-09-16 13:25:14 +0300

    add function for form submission

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { FormEvent, useRef} from 'react'
 import { SectionTitle } from '../../../components/SectionTitle'
 import { Button } from '../../../components/Button'
 import { Container } from '../../../components/Container'
 import { S } from './Contacts_Styles'
 
+import axios from 'axios'
 
 export const Contacts: React.FC = () => {
+    const formRef = useRef<HTMLFormElement | null>(null)
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (formRef.current) {
+            const formData = new FormData(formRef.current)
+            const data = Object.fromEntries(formData)
+
+            try {
+                await axios.post('http://localhost:5000/sendMessage', data)
+                formRef.current.reset()
+            } catch (error) {
+    
+            }
+        }
+    }
+    
     return (
         <S.Contacts id='contact'>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <S.Form>
-                    <S.Field placeholder="name" />
-                    <S.Field placeholder='your contact details'/>
-                    <S.Field placeholder="subject" />
-                    <S.Field placeholder='message' as="textarea" />
+                
+                <S.Form ref={formRef} onSubmit={handleSubmit}>
+                    <S.Field placeholder="name" name='name' required/>
+                    <S.Field placeholder='your contact details' name='contactDetails' required/>
+                    <S.Field placeholder="subject" name='subject' required/>
+                    <S.Field placeholder='message' as="textarea" name='message' required/>
                     <Button type='submit'>Submit</Button>
                 </S.Form>
             </Container>
