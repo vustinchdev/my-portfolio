2de53430e06551c51d8b868c4512f74a0335ae4a
vustinchdev <vustinchdev@gmail.com>
2024-09-16 16:27:24 +0300

    add urls to links

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,12 +6,15 @@ import { S } from './Footer_ Styles'
 const socialItemData = [
   {
     iconId: 'mail',
+    url: 'mailto:vustinchdev@gmail.com'
   },
   {
     iconId: 'linkedin',
+    url: 'https://www.linkedin.com/in/%D0%B2%D0%B0%D0%B4%D0%B8%D0%BC-%D1%83%D1%81%D1%82%D0%B8%D0%BD%D1%87%D0%B8%D0%BA-31bb65286/'
   },
   {
     iconId: 'github',
+    url: 'https://github.com/vustinchdev'
   },
 ]
 
@@ -23,7 +26,7 @@ export const Footer: React.FC = () => {
           {socialItemData.map((s, index) => {
             return (
               <S.SocialItem key={index}>
-                <S.SocialLink>
+                <S.SocialLink href={s.url} target='_blank'>
                   <Icon iconId={s.iconId} width="39" height="38" viewBox="0 0 39 38" />
                 </S.SocialLink>
               </S.SocialItem>
