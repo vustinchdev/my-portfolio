b666eed3a8d5be9f89a87a51e8e2b8d9fa82f7bc
vustinchdev <vustinchdev@gmail.com>
2024-09-18 12:45:56 +0300

    change url linkedin

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -10,7 +10,7 @@ const socialItemData = [
   },
   {
     iconId: 'linkedin',
-    url: 'https://www.linkedin.com/in/%D0%B2%D0%B0%D0%B4%D0%B8%D0%BC-%D1%83%D1%81%D1%82%D0%B8%D0%BD%D1%87%D0%B8%D0%BA-31bb65286/'
+    url: 'https://www.linkedin.com/in/vadim-ustinchik/'
   },
   {
     iconId: 'github',
