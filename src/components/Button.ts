6f69311d98cf28bf7580915df5f348b1cb204c0a
vustinchdev <vustinchdev@gmail.com>
2024-12-30 17:02:56 +0300

    feat: add link for download CV

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -2,11 +2,13 @@ import styled from 'styled-components'
 import { theme } from 'styles/Theme'
 
 export const Button = styled.button`
-  width: 134px;
-  height: 45px;
+  font-size: 16px;
+  font-weight: bold;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 15px;
   padding: 12px 20px;
   border-radius: 6px;
   background-color: ${theme.colors.accent};
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -5,6 +5,8 @@ import { Container } from 'components/Container'
 import { S } from './Main_Styles'
 import Typewriter from 'typewriter-effect'
 import Tilt from 'react-parallax-tilt'
+import { Button } from 'components/Button'
+import { Icon } from 'components/icon/Icon'
 
 export const Main: React.FC = () => {
   return (
@@ -23,6 +25,10 @@ export const Main: React.FC = () => {
               />
             </S.MainTitle>
             <S.Name>Vadim Ustinchik</S.Name>
+            <Button as={'a'} href='#' target='_blank' rel="noopener noreferrer" download>
+              <Icon iconId='fileDownload' width="24" height="24" viewBox="0 0 24 24"/>
+              Download CV
+            </Button>
           </div>
           <Tilt>
             <S.Photo src={photo} alt="photo" />
